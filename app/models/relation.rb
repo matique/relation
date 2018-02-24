ab5de693f770d09c9f6eb9161f50702894f6973b
matique <dittmar.krall@matique.de>
2018-02-24 17:33:10 +0100

    'cleaning'

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -11,20 +11,10 @@ class Relation < ActiveRecord::Base
   end
 
   def self.references_raw(name, id_from)
-    ids = self.referenced_ids_raw(name, id_from)
-  end
-
-  def self.followers_raw(name, id_to)
-    ids = self.followers_ids_raw(name, id_to)
-  end
-
-
- private
-  def self.referenced_ids_raw(name, id_from)
     Relation.where(name: name, x_id: id_from).pluck(:y_id)
   end
 
-  def self.followers_ids_raw(name, id_to)
+  def self.followers_raw(name, id_to)
     Relation.where(name: name, y_id: id_to).pluck(:x_id)
   end
 
