70960db1ff9c844a76f5f27163d5df328c204707
matique <dittmar.krall@matique.de>
2018-02-25 14:31:02 +0100

    '...'

--- app/models/relation_ext.rb
+++ app/models/relation_ext.rb
@@ -12,47 +12,37 @@ class Relation < ActiveRecord::Base
   end
 
   def self.references(row, kind)
-    ref_foll(row, kind) { |klass, row|
-      references_ids(row, klass)
-    }
-  end
-
-  def self.followers(kind, row)
-    ref_foll(row, kind) { |klass, row|
-      followers_ids(klass, row)
-    }
-  end
-
- private
-  def self.ref_foll(row, kind, &proc)
-    klass = kind
-    klass = kind.constantize  unless klass.kind_of?(Class)
-    ids = proc.call(klass, row)
+    klass, name, id_from = normalize2(kind, row)
+    name = "#{name} #{klass.name}"
+    ids = references_raw(name, id_from)
     klass.where(id: ids)
   end
 
+  def self.followers(kind, row)
+    klass, name, id_to = normalize2(kind, row)
+    name = "#{klass.name} #{name}"
+    ids = followers_raw(name, id_to)
+    klass.where(id: ids)
+  end
+
+ private
   def self.name_id(resource)
     raise 'missing resource'  unless resource
     [resource.class.name, resource.id]
   end
 
-def self.normalize(row_from, row_to)
-  name_from, id_from = name_id(row_from)
-  name_to, id_to     = name_id(row_to)
-  name = "#{name_from} #{name_to}"
-  { name: name, x_id: id_from, y_id: id_to }
-end
-
-
-  def self.references_ids(row, klass)
-    name_from, id_from = name_id(row)
-    name = "#{name_from} #{klass.name}"
-    Relation.where(name: name, x_id: id_from).pluck(:y_id)
+  def self.normalize(row_from, row_to)
+    name_from, id_from = name_id(row_from)
+    name_to, id_to     = name_id(row_to)
+    name = "#{name_from} #{name_to}"
+    { name: name, x_id: id_from, y_id: id_to }
   end
 
-  def self.followers_ids(klass, row)
-    name_to, id_to = name_id(row)
-    name = "#{klass.name} #{name_to}"
-    Relation.where(name: name, y_id: id_to).pluck(:x_id)
+  def self.normalize2(kind, row)
+    klass = kind
+    klass = kind.constantize  unless klass.kind_of?(Class)
+    name, id = name_id(row)
+    [klass, name, id]
   end
+
 end
