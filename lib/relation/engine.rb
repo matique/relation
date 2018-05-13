425b64d2fcc2b67e2ab038e6920f42ab1e6fe8fd
matique <dittmar.krall@matique.de>
2018-05-13 17:36:15 +0200

    '...'

--- lib/relation/engine.rb
+++ lib/relation/engine.rb
@@ -3,16 +3,10 @@ module ModRelation
     isolate_namespace ModRelation
 
 # # https://github.com/rails/rails/issues/22261
-# p 11111111111111111111111
 #     initializer :append_migrations do |app|
-# #p [10, app]
-# p 10
 #       config.paths['db/migrate'].expanded.each do |path|
-# p [12, path]
 #         app.config.paths['db/migrate'] << path
-# p 13
 #         ActiveRecord::Migrator.migrations_paths << path
-# p [14, ActiveRecord::Migrator.migrations_paths]
 #       end
 #     end
 
