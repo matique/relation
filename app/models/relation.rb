87af1702ed2693c1fac951768ae0d02381f80976
matique <dittmar.krall@matique.de>
2018-06-12 18:31:19 +0200

    save

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,21 +1,21 @@
 class Relation < ActiveRecord::Base
 
-  def self.add_raw(name, id_from, id_to)
-    hsh = { name: name, x_id: id_from, y_id: id_to }
+  def self.add_raw(name, id_from, id_to, position = 0)
+    hsh = { name: name, from_id: id_from, to_id: id_to, position: position }
     Relation.create!(hsh)  if Relation.where(hsh).first == nil
   end
 
   def self.delete_raw(name, id_from, id_to)
-    hsh = { name: name, x_id: id_from, y_id: id_to }
+    hsh = { name: name, from_id: id_from, to_id: id_to }
     Relation.where(hsh).delete_all
   end
 
   def self.references_raw(name, id_from)
-    Relation.where(name: name, x_id: id_from).pluck(:y_id)
+    Relation.where(name: name, from_id: id_from).pluck(:to_id)
   end
 
   def self.followers_raw(name, id_to)
-    Relation.where(name: name, y_id: id_to).pluck(:x_id)
+    Relation.where(name: name, to_id: id_to).pluck(:from_id)
   end
 
 end
