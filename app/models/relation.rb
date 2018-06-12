eeb4d547bb69b0220120dab0310f024eeb6b5aab
matique <dittmar.krall@matique.de>
2018-06-12 18:41:25 +0200

    'renaming'

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,21 +1,21 @@
 class Relation < ActiveRecord::Base
 
-  def self.add_raw(name, id_from, id_to, position = 0)
-    hsh = { name: name, from_id: id_from, to_id: id_to, position: position }
+  def self.add_raw(name, from_id, to_id, position = 0)
+    hsh = { name: name, from_id: from_id, to_id: to_id, position: position }
     Relation.create!(hsh)  if Relation.where(hsh).first == nil
   end
 
-  def self.delete_raw(name, id_from, id_to)
-    hsh = { name: name, from_id: id_from, to_id: id_to }
+  def self.delete_raw(name, from_id, to_id)
+    hsh = { name: name, from_id: from_id, to_id: to_id }
     Relation.where(hsh).delete_all
   end
 
-  def self.references_raw(name, id_from)
-    Relation.where(name: name, from_id: id_from).pluck(:to_id)
+  def self.references_raw(name, from_id)
+    Relation.where(name: name, from_id: from_id).pluck(:to_id)
   end
 
-  def self.followers_raw(name, id_to)
-    Relation.where(name: name, to_id: id_to).pluck(:from_id)
+  def self.followers_raw(name, to_id)
+    Relation.where(name: name, to_id: to_id).pluck(:from_id)
   end
 
 end
