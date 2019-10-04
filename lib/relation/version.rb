250fd1cca43a9a8896601c1dc7992730dc130585
matique <dittmar.krall@matique.de>
2019-10-04 11:25:58 +0200

    'v0.3.4'

--- lib/relation/version.rb
+++ lib/relation/version.rb
@@ -1,6 +1,6 @@
 module ModRelation
-#  VERSION = '0.3.4'  # 2019-10-04
-  VERSION = '0.3.3'  # 2019-03-23
+  VERSION = '0.3.4'  # 2019-10-04
+#  VERSION = '0.3.3'  # 2019-03-23
 #  VERSION = '0.3.2'  # 2019-03-05
 #  VERSION = '0.3.1'  # 2018-08-05
 #  VERSION = '0.3.0'  #
