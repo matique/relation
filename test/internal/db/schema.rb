a67a7a4a4c5adb05076673f364e657c744339aa6
dittmar <dittmar.krall@matique.de>
2023-01-18 09:31:44 +0100

    'lib/relation/engine.rb added; table relations'

--- test/internal/db/schema.rb
+++ test/internal/db/schema.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 ActiveRecord::Schema.define(version: 202208) do
+  create_table :relations, force: true, id: false do |t|
+    t.string     :name
+    t.references :from, null: false
+    t.references :to, null: false
+    t.timestamps
+
+    t.index :name
+  end
+
   create_table "orders", force: true do |t|
     t.string :category
     t.text :bag
