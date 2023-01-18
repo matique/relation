41117813f177c4a4567e447db087cd7455d086ad
dittmar <dittmar.krall@matique.de>
2023-01-18 09:56:40 +0100

    'adde test/internal/app/models/user.rb'

--- test/internal/db/schema.rb
+++ test/internal/db/schema.rb
@@ -11,7 +11,7 @@ ActiveRecord::Schema.define(version: 202208) do
   end
 
   create_table "orders", force: true do |t|
-    t.string :category
+    t.string :name
     t.text :bag
 
     t.datetime "created_at", null: false
@@ -19,8 +19,7 @@ ActiveRecord::Schema.define(version: 202208) do
   end
 
   create_table "users", force: true do |t|
-    t.string :type
-    t.text :bag
+    t.string :email
 
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
