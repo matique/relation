7189bf87be0b1680b8eb9521349cb12b9c2ecedd
dittmar <dittmar.krall@matique.de>
2023-01-18 14:33:16 +0100

    'test/relation_test.rb running; dirty'

--- lib/relation.rb
+++ lib/relation.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
-#class Relation < ApplicationRecord
 class Relation < ActiveRecord::Base
-p 111111133
-#ic Relation.public_methods(false).sort
-  def self.add_raw
-p 2222222222
+  def self.add_raw(name, from_id, to_id)
+    hsh = {name: name, from_id: from_id, to_id: to_id}
+    Relation.create!(hsh) if Relation.where(hsh).first.nil?
+  end
+
+  def self.delete_raw(name, from_id, to_id)
+    hsh = {name: name, from_id: from_id, to_id: to_id}
+    Relation.where(hsh).delete_all
+  end
+
+  def self.references_raw(name, from_id)
+    Relation.where(name: name, from_id: from_id).pluck(:to_id)
+  end
+
+  def self.followers_raw(name, to_id)
+    Relation.where(name: name, to_id: to_id).pluck(:from_id)
   end
 end
 
--- test/relation_test.rb
+++ test/relation_test.rb
@@ -20,39 +20,39 @@ describe Relation do
     end
   end
 
-#  it "should delete a relation" do
-#    Relation.add_raw :raw, u2_id, u_id
-#    assert_difference("Relation.count", -1) do
-#      Relation.delete_raw :raw, u2_id, u_id
-#    end
-#  end
-#
-#  it "should return referenced ids" do
-#    arr = Relation.references_raw(:rel, u_id)
-#    assert_equal [o_id].sort, arr.sort
-#
-#    arr = Relation.references_raw(:rel, u2_id)
-#    assert_equal [o_id, o2_id].sort, arr.sort
-#  end
-#
-#  it "should return followers ids" do
-#    arr = Relation.followers_raw(:rel, o_id)
-#    assert_equal [u_id, u2_id].sort, arr.sort
-#
-#    arr = Relation.followers_raw(:rel, o2_id)
-#    assert_equal [u2_id].sort, arr.sort
-#  end
-#
-#  it "should not add twice the same connection" do
-#    assert_difference("Relation.count") do
-#      Relation.add_raw :rel, u2_id, unknown_id
-#      Relation.add_raw :rel, u2_id, unknown_id
-#    end
-#  end
-#
-#  it "should handle unexistent connection" do
-#    assert_difference("Relation.count", 0) do
-#      Relation.delete_raw :rel, u2_id, unknown_id
-#    end
-#  end
+  it "should delete a relation" do
+    Relation.add_raw :raw, u2_id, u_id
+    assert_difference("Relation.count", -1) do
+      Relation.delete_raw :raw, u2_id, u_id
+    end
+  end
+
+  it "should return referenced ids" do
+    arr = Relation.references_raw(:rel, u_id)
+    assert_equal [o_id].sort, arr.sort
+
+    arr = Relation.references_raw(:rel, u2_id)
+    assert_equal [o_id, o2_id].sort, arr.sort
+  end
+
+  it "should return followers ids" do
+    arr = Relation.followers_raw(:rel, o_id)
+    assert_equal [u_id, u2_id].sort, arr.sort
+
+    arr = Relation.followers_raw(:rel, o2_id)
+    assert_equal [u2_id].sort, arr.sort
+  end
+
+  it "should not add twice the same connection" do
+    assert_difference("Relation.count") do
+      Relation.add_raw :rel, u2_id, unknown_id
+      Relation.add_raw :rel, u2_id, unknown_id
+    end
+  end
+
+  it "should handle unexistent connection" do
+    assert_difference("Relation.count", 0) do
+      Relation.delete_raw :rel, u2_id, unknown_id
+    end
+  end
 end
